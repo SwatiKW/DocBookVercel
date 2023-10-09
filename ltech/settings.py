47ab70dffc21fecfd6d251d7a84f6ffa42fce588
SwatiKW <swatiansavy@outlook.com>
2023-10-09 21:26:17 +0530

    vercel config added

--- ltech/settings.py
+++ ltech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fy373g6g!5k5341ieoj!(18xke5cmjfd!7fje8aqp#q5q(u6vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
